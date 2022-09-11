00e5c75651775423b985550784df6aac35a7ef0b
Adam <adamschroeder.m@gmail.com>
2022-09-11 16:19:59 -0400

    Update app3.py

--- pages/app3.py
+++ pages/app3.py
@@ -7,17 +7,17 @@ dash.register_page(__name__)
 
 def layout():
     return html.Div([
-    dbc.Row(
-        [
-            dbc.Col(
-                [
-                    sidebar()
-                ], xs=4, sm=4, md=2, lg=2, xl=2, xxl=2),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        sidebar()
+                    ], xs=4, sm=4, md=2, lg=2, xl=2, xxl=2),
 
-            dbc.Col(
-                [
-                    html.H3('App 3 - Build your own app here and let me know if you have questions.', style={'textAlign':'center'}),
-                ], xs=8, sm=8, md=10, lg=10, xl=10, xxl=10)
-        ]
-    )
-])
+                dbc.Col(
+                    [
+                        html.H3('App 3 - build your own app here and let me know if you have questions', style={'textAlign':'center'}),
+                    ], xs=8, sm=8, md=10, lg=10, xl=10, xxl=10)
+            ]
+        )
+    ])
